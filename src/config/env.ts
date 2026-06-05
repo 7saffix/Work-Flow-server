f943d88ea35d93e6eefc018237a7eb96f4524e35
safi <7saffix@gmail.com>
2026-06-05 16:25:13 +0600

    vercel config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,24 +1,44 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const requiredEnv: string[] = [
-  "MONGO_URI",
-  "PORT",
-  "JWT_ACCESS_SECRET",
-  "JWT_REFRESH_SECRET",
-  "JWT_ACCESS_EXPIRES",
-  "JWT_REFRESH_EXPIRES",
-  "FRONTEND_URL",
-];
+interface EnvConfig {
+  PORT: string;
+  MONGO_URI: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_REFRESH_EXPIRES: string;
+  FRONTEND_URL: string;
+}
 
-let envVars: Record<string, string> = {};
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables: string[] = [
+    "MONGO_URI",
+    "PORT",
+    "JWT_ACCESS_SECRET",
+    "JWT_REFRESH_SECRET",
+    "JWT_ACCESS_EXPIRES",
+    "JWT_REFRESH_EXPIRES",
+    "FRONTEND_URL",
+  ];
 
-requiredEnv.forEach((key) => {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Missing env variable: ${key}`);
-  }
-  envVars[key] = value;
-});
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variable ${key}`);
+    }
+  });
 
+  return {
+    PORT: process.env.PORT as string,
+    MONGO_URI: process.env.MONGO_URI as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+  };
+};
+
+const envVars = loadEnvVariables();
 export default envVars;
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import app from "./app";
 import envVars from "./config/env";
 
 let server: Server;
+let PORT = envVars.PORT || 3000;
 
 const startServer = async () => {
   try {
     await mongoose.connect(envVars.MONGO_URI);
     console.log("Database Connected SuccessFully");
-    server = app.listen(envVars.PORT, () => {
-      console.log(`Server running at port ${envVars.PORT}💻`);
+    server = app.listen(PORT, () => {
+      console.log(`Server running at port ${PORT}💻`);
     });
   } catch (error) {
     console.log("Database connection failed❗");
